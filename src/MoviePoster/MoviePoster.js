f521c6820ad103ade8915545fea3ed9e47ce7424
Kristin Weiland <kristinweiland@gmail.com>
2025-02-14 13:30:17 -0800

    Add an upvote and down vote button and corresponding image, adjust style for buttons and posters.

--- src/MoviePoster/MoviePoster.js
+++ src/MoviePoster/MoviePoster.js
@@ -1,10 +1,20 @@
 import './MoviePoster.css';
+import upvoteIcon from '../icons/upvote.png';
+import downvoteIcon from '../icons/downvote.png';
 
 function MoviePoster({ title, vote_count, image, id}) {
   return (
     <section className='MoviePoster'>
       <img src={image} alt={title} />
-      <h4>{vote_count}</h4>
+      <div className='Votes'>
+        <button>
+          <img  className='VoteImage Up' src={upvoteIcon} alt='upVote'/>
+        </button>
+        <p>{vote_count}</p>
+        <button>
+          <img className='VoteImage Down' src={downvoteIcon} alt='downVote' />
+        </button>
+      </div>
     </section>
   );
 }
