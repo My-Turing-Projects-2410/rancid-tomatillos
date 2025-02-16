10a096bd86b8acb59cf0d89c63c98fb3f890b1b9
elysableu <elysabward@gmail.com>
2025-02-15 19:38:17 -0700

    Add styling to movie details

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -12,9 +12,9 @@ function MovieDetails({details}) {
 
   return (
     <section className='MovieDetails'>
-      <img className='Backdrop' src={details.backdrop_path}/>
+      <img src={details.backdrop_path} alt={details.title}/>
       <div className='Description'>
-        <h2 className='Title'>{details.title}</h2>
+        <h2>{details.title}</h2>
         <div className='Genres'>
           {formatGenres(details.genre_ids)}
         </div>
