a384ee7861cf5c5adee7355a9e21175fa830666e
Kristin Weiland <kristinweiland@gmail.com>
2025-02-19 21:08:05 -0800

    clean up

--- cypress/e2e/movie_details_spec.cy.js
+++ cypress/e2e/movie_details_spec.cy.js
@@ -1,27 +1,13 @@
+import MovieDetails from "../../src/MovieDetails/MovieDetails";
+
 describe('Movie Details View flow', () => {
   beforeEach(() => {
-    // Login the user and stub the network requests 
     cy.intercept('GET', 'https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/826510', {
       statusCode: 200, 
       fixture: "movie_details.json"
     })
+    
     cy.visit('http://localhost:3000');
-
   })  
-
-    it('displays the application title', () => {
-      cy.get('h1').should('exist')
-      cy.get('h1').should('be.visible').and('contain', 'rancid tomatillos')
-    });
-
-    // it('displays the movie title', () => {
-    //   cy.get('h2').should('be.visible').and('contain', 'Harold and the Purple Crayon')
-    // });
-
-    // it('displays the movie genres', () => {
-    //   cy.get('div.Genres p.genre').should('be.visible').and('contain', 'Adventure')
-    //   .genre 
-    // });
-
 
 })
